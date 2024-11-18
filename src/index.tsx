f17c2d9d0c96bb2c89813f43bef7a0d59c39980e
himanushi <himanushi@users.noreply.github.com>
2024-11-18 14:16:25 +0900

    無駄なコードの削除と簡素化

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,21 @@
-import {
-  ColorModeScript,
-  ColorModeWithSystem,
-  ThemeSchemeScript,
-  defaultConfig,
-} from "@yamada-ui/react"
-import { Hono } from "hono"
-import { getCookie } from "hono/cookie"
-import { renderToString } from "react-dom/server"
+import { Hono } from "hono";
+import { renderToString } from "react-dom/server";
 
-const app = new Hono()
+const app = new Hono();
 
 app.get("*", (c) => {
-  const colorMode = getCookie(c, "ui-color-mode") as
-    | ColorModeWithSystem
-    | undefined
-  const themeScheme = getCookie(c, "ui-theme-scheme")
-
-  const initialColorMode = colorMode ?? defaultConfig.initialColorMode
-  const initialThemeScheme = themeScheme ?? defaultConfig.initialThemeScheme
-
   return c.html(
     renderToString(
       <html lang="ja">
         <head>
-          <script type="module" src="/src/client.tsx"></script>
+          <script type="module" src="/src/client.tsx" />
         </head>
-
         <body>
-          <ColorModeScript
-            type="cookie"
-            nonce="testing"
-            initialColorMode={initialColorMode}
-          />
-          <ThemeSchemeScript
-            type="cookie"
-            nonce="testing"
-            initialThemeScheme={initialThemeScheme}
-          />
-          <div id="root"></div>
+          <div id="root" />
         </body>
       </html>,
     ),
-  )
-})
+  );
+});
 
-export default app
+export default app;
