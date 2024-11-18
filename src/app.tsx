c370426be8af7c25dbcd1306859f952f0e7376bd
himanushi <himanushi@users.noreply.github.com>
2024-11-18 14:16:11 +0900

    import文の順序変更とセミコロン追加

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react"
-import { Button } from "@yamada-ui/react"
+import { Button } from "@yamada-ui/react";
+import type { FC } from "react";
 
 const App: FC = () => {
-  return <Button>Click me!</Button>
-}
+  return <Button>Click me!</Button>;
+};
 
-export default App
+export default App;
