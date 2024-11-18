5a34de7dc23374af754c9444a7e983d10520ac23
himanushi <himanushi@users.noreply.github.com>
2024-11-18 14:01:46 +0900

    ファイルコピースクリプトの作成

---
+++ scripts/copyFiles.js
@@ -0,0 +1,89 @@
+const description =
+  "hono, CloudFlare Pages & D1, drizzle, react を使用したアプリを開発しています。";
+
+import fs from "node:fs";
+import path from "node:path";
+import clipboardy from "clipboardy";
+
+// コピペから除外するファイル又はディレクトリ
+const excludeFiles = [
+  "node_modules",
+  ".git",
+  ".wrangler",
+  "dist",
+  "drizzle",
+  ".github",
+  ".vscode",
+  "biome.json",
+  ".DS_Store",
+  "Thumbs.db",
+  "package-lock.json",
+  "favicon.ico",
+  "tsconfig.json",
+  "vite.config.ts",
+  "vite-env.d.ts",
+  "tsconfig.node.json",
+  "static",
+  ".env",
+  ".env.local",
+  "README.md",
+  ".keep",
+  "wrangler.toml",
+  "meta",
+];
+
+function directoryContents(dir, depth = 0) {
+  let output = "";
+
+  const files = fs.readdirSync(dir);
+
+  for (const file of files) {
+    if (excludeFiles.includes(file)) continue;
+
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      output += `${" ".repeat(depth * 2)}Dir: ${file}\n`;
+      output += directoryContents(filePath, depth + 1);
+    } else {
+      output += `${" ".repeat(depth * 2)}File: ${file}\n${fs.readFileSync(
+        filePath,
+        "utf8",
+      )}\n`;
+    }
+  }
+
+  return output;
+}
+
+const excludeTreeFiles = ["node_modules", ".git", "dist", ".DS_Store"];
+
+function tree(dir, indent = 0) {
+  let output = "";
+  const files = fs.readdirSync(dir);
+
+  for (const file of files) {
+    if (excludeTreeFiles.includes(file)) continue;
+
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    output += `${" ".repeat(indent * 2)}${
+      stat.isDirectory() ? "Dir" : "File"
+    }: ${file}\n`;
+
+    if (stat.isDirectory()) {
+      output += tree(filePath, indent + 1);
+    }
+  }
+
+  return output;
+}
+
+const treeOutput = tree(process.cwd());
+const contents = directoryContents(process.cwd());
+const combinedOutput = `${description}\n\n${treeOutput}\n\n${contents}`;
+
+clipboardy.writeSync(combinedOutput);
+console.log("コピーしました");
